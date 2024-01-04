8ae6092a0b6c55275ba8d8ebd02296ea85f11594
yousuf-ahmed-sr <90528189+yousuf-ahmed-sr@users.noreply.github.com>
2024-01-04 18:37:27 -0500

    Update test5.py

--- test5.py
+++ test5.py
@@ -1,4 +1,23 @@
 import turtle
 import keyboard
+import mouse
 turtle.Screen()
 drawer=turtle.Turtle()
+
+while True:
+    if keyboard.is_pressed('up'):
+        drawer.forward(1.5)
+    if keyboard.is_pressed('right'):
+        drawer.right(5)
+        if keyboard.is_pressed('up'):
+             drawer.forward(5)
+    if keyboard.is_pressed('left'):
+        drawer.left(5)
+        if keyboard.is_pressed('up'):
+             drawer.forward(5)
+             
+    if mouse.is_pressed():
+        break
+    if keyboard.is_pressed('space'):
+        drawer.clear()
+  
